8999f9a445e485806a40417cab7e00f82aaccfc0
Nathaniel Christian <nathaniel.christian@gmail.com>
2025-08-26 00:39:16 +0700

    feat: add sf-pro font files

--- src/lib/fonts.ts
+++ src/lib/fonts.ts
@@ -2,7 +2,7 @@ import localFont from 'next/font/local';
 import { Geist, Poppins, Inter } from "next/font/google";
 
 export const geistSans = Geist({
-    variable: "--font-geist-sans",
+    variable: '--font-geist-sans',
     subsets: ["latin"],
 });
 
